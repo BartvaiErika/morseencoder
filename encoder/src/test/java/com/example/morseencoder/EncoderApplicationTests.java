32d505bec64b01b2fd45a12a1b0201780819224a
BartvaiErika <erika.bartvai@gmail.com>
2019-05-08 11:44:31 +0200

    first commit morseencoder for ci

--- encoder/src/test/java/com/example/morseencoder/EncoderApplicationTests.java
+++ encoder/src/test/java/com/example/morseencoder/EncoderApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.morse;
+package com.example.morseencoder;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class MorsePartForMorseencoderApplicationTests {
+public class EncoderApplicationTests {
 
   @Test
   public void contextLoads() {
